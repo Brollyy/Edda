2daf87cc0fd39792a0289eb4527a290fbbf7af55
Brollyy <neon349@gmail.com>
2026-03-21 19:34:03 +0100

    Add WPF UI driver placeholder

---
+++ tests/Edda.Wpf.UI.Tests/WpfUIDriver.cs
@@ -0,0 +1,44 @@
+using Edda.UI.Harness;
+
+namespace Edda.Wpf.UI.Tests;
+
+public class WpfUIDriver : IUIDriver
+{
+    public void Launch() { }
+
+    public void Shutdown() { }
+
+    public void WaitForIdle() { }
+
+    public void ClickButton(string id) { }
+
+    public void SetText(string id, string value) { }
+
+    public string GetText(string id) => string.Empty;
+
+    public bool IsVisible(string id) => false;
+
+    public bool IsEnabled(string id) => false;
+
+    public bool IsChecked(string id) => false;
+
+    public void ToggleCheckbox(string id, bool value) { }
+
+    public string GetSelectedValue(string id) => string.Empty;
+
+    public void SelectDropdown(string id, string value) { }
+
+    public void OpenMenu(string id) { }
+
+    public void SelectMenuItem(string path) { }
+
+    public void SendKeyboardShortcut(string shortcut) { }
+
+    public void Drag(string sourceId, string targetId) { }
+
+    public void InvokeCommand(string commandId) { }
+
+    public void SetTestFileSelection(string path) { }
+
+    public void AssertNotificationContains(string text) { }
+}
